7586e09cd94ebff8ff2a0238d167d7bb3641043b
Cherry <cherry@sparklet.org>
2025-07-02 07:56:38 -0400

    add ctrl shift s for noa w

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -292,6 +292,7 @@ return {
                 end,
             })
 
+            -- TODO use this instead of the deprecated one
             -- Change diagnostic symbols in the sign column (gutter)
             -- if vim.g.have_nerd_font then
             --   local signs = { ERROR = '', WARN = '', INFO = '', HINT = '' }
@@ -445,7 +446,7 @@ return {
                 -- have a well standardized coding style. You can add additional
                 -- languages here or re-enable it for the disabled ones.
                 -- local disable_filetypes = { c = true, cpp = true }
-                local disable_filetypes = { }
+                local disable_filetypes = {}
                 local lsp_format_opt
                 if disable_filetypes[vim.bo[bufnr].filetype] then
                     lsp_format_opt = 'never'

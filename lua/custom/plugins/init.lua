6345ab23a2e42e97a23de9cb57d5ae08b0cce759
Cherry <cherry@sparklet.org>
2025-07-26 17:19:46 -0400

    fixed for real this time (force push)

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -424,25 +424,25 @@ return {
                 vim.lsp.enable({ 'vue_ls' }),
             }
 
-            -- -- FUCK OFF!!!!!!!!!!!!! (IT WON'T LET ME USE VUE FILETYPE IN MASON-LSPCONFIG FOR SOME REASON?!!!!)
-            -- require("lspconfig").vtsls.setup({
-            --     filetypes = { 'vue', 'typescript', 'javascript', 'javascriptreact', 'typescriptreact' },
-            --     settings = {
-            --         vtsls = {
-            --             tsserver = {
-            --                 globalPlugins = {
-            --                     {
-            --                         name = '@vue/typescript-plugin',
-            --                         location = vim.fn.stdpath('data') ..
-            --                             "/mason/packages/vue-language-server/node_modules/@vue/language-server",
-            --                         languages = { 'vue' },
-            --                         configNamespace = 'typescript',
-            --                     }
-            --                 },
-            --             },
-            --         },
-            --     },
-            -- })
+            -- FUCK OFF!!!!!!!!!!!!! (IT WON'T LET ME USE VUE FILETYPE IN MASON-LSPCONFIG FOR SOME REASON?!!!!)
+            require("lspconfig").vtsls.setup({
+                filetypes = { 'vue', 'typescript', 'javascript', 'javascriptreact', 'typescriptreact' },
+                settings = {
+                    vtsls = {
+                        tsserver = {
+                            globalPlugins = {
+                                {
+                                    name = '@vue/typescript-plugin',
+                                    location = vim.fn.stdpath('data') ..
+                                        "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+                                    languages = { 'vue' },
+                                    configNamespace = 'typescript',
+                                }
+                            },
+                        },
+                    },
+                },
+            })
         end,
     },
 

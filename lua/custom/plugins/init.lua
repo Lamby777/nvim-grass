d0777e157cc50c8994f73e3374a8f70e0b9c0df4
Cherry <cherry@sparklet.org>
2025-08-19 06:46:40 -0400

    add trouble.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -214,6 +214,49 @@ return {
     },
 
     {
+        "folke/trouble.nvim",
+        opts = {
+            focus = true,
+            auto_close = true,
+            -- auto_open = true,
+            warn_no_results = false,
+        },
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>l",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            -- {
+            --     "<leader>xX",
+            --     "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            --     desc = "Buffer Diagnostics (Trouble)",
+            -- },
+            -- {
+            --     "<leader>cs",
+            --     "<cmd>Trouble symbols toggle focus=false<cr>",
+            --     desc = "Symbols (Trouble)",
+            -- },
+            -- {
+            --     "<leader>cl",
+            --     "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            --     desc = "LSP Definitions / references / ... (Trouble)",
+            -- },
+            -- {
+            --     "<leader>xL",
+            --     "<cmd>Trouble loclist toggle<cr>",
+            --     desc = "Location List (Trouble)",
+            -- },
+            -- {
+            --     "<leader>xQ",
+            --     "<cmd>Trouble qflist toggle<cr>",
+            --     desc = "Quickfix List (Trouble)",
+            -- },
+        },
+    },
+
+    {
         -- Main LSP Configuration
         'neovim/nvim-lspconfig',
         dependencies = {
